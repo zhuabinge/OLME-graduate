c766e625af6cdbf22e084756bca66210598a870f
bingo <bingo_zhang520@163.com>
2014-12-28 19:28:46 +0800

    重新整合app代码

--- app/src/main/java/com/olme/activity/MainActivity.java
+++ app/src/main/java/com/olme/activity/MainActivity.java
@@ -1,5 +1,8 @@
 package com.olme.activity;
 import android.app.Activity;
+import android.content.Intent;
+
+import com.olme.application.ExitApplication;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
@@ -8,7 +11,18 @@ import org.androidannotations.annotations.EActivity;
 @EActivity(R.layout.activity_main)
 public class MainActivity extends Activity {
 
+
     @AfterViews
-    void init(){}
+    void init(){
+        ExitApplication.getInstance().addActivity(this);
+        try {
+
+            Intent intent = new Intent(this, LoginActivity_.class);
+            Thread.sleep(10000);
+            startActivity(intent);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
 }
